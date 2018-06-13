63b44e9b445a5704cf3060f3a1787fe20905afe5
Sky Young <sky@alexandria.io>
2018-06-13 11:29:36 -0700

    Attach line number refs

--- static/scripts/linenumber.js
+++ static/scripts/linenumber.js
@@ -9,16 +9,24 @@
 
   if (source && source[0]) {
     anchorHash = document.location.hash.substring(1);
-    lines = source[0].getElementsByTagName('li');
+    lines = source[0].getElementsByTagName('tr');
     totalLines = lines.length;
 
     for (; i < totalLines; i++) {
       lineNumber++;
       lineId = 'line' + lineNumber;
       lines[i].id = lineId;
+      lines[i].classList.add('linenums');
+      lines[i].dataset.href = window.location.href.split('#')[0] + "#" + lineId
       if (lineId === anchorHash) {
         lines[i].className += ' selected';
       }
     }
   }
 })();
+
+jQuery(document).ready(function($) {
+    $(".linenums").click(function() {
+        window.location = $(this).data("href");
+    });
+});
